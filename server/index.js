c82f6bb307aef9f3a22dd12bafa824b56bda47eb
James Colestock <james@colestock.com>
2019-10-01 22:09:09 -0600

    Added support for new reelic

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const sslRedirect = require("heroku-ssl-redirect");
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const keys = require("./config/keys");
-const errorHandler = require("./middlewares/errorHandler");
+require('newrelic');
+const sslRedirect = require('heroku-ssl-redirect');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const keys = require('./config/keys');
+const errorHandler = require('./middlewares/errorHandler');
 
-require("./models/User"); // Used in match routes, etc.
-require("./models/Match"); // Used in match routes, etc.
-require("./models/Ping"); // Used in ping routes, etc.
+require('./models/User'); // Used in match routes, etc.
+require('./models/Match'); // Used in match routes, etc.
+require('./models/Ping'); // Used in ping routes, etc.
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
@@ -21,18 +22,18 @@ const app = express();
 app.use(sslRedirect());
 
 //app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
-require("./routes/matchRoutes")(app);
-require("./routes/pingRoutes")(app);
+require('./routes/matchRoutes')(app);
+require('./routes/pingRoutes')(app);
 
 app.use(errorHandler); // Custom default, i.e., catch-all, error handler middleware
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
