6abe45813e1e1268df0fe10f69b5d4b3fd50a800
James Colestock <james@colestock.com>
2020-01-29 13:39:20 -0700

    More cache testing

--- server/routes/matchRoutes.js
+++ server/routes/matchRoutes.js
@@ -5,8 +5,9 @@ module.exports = (app, memcache) => {
   app.get('/api/match/:id', async (req, res, next) => {
     try {
       // throw new Error('Testing match error...');
-      const matchTry = await memcache.get(`match-${req.params.id}`);
-      console.log(`match-${req.params.id}`, matchTry);
+      const cacheKey = `match-${req.params.id}`;
+      const cached = await memcache.get(cacheKey);
+      if (cached) return res.send(cached);
       let match = await Match.findOne({
         matchId: req.params.id
       }).populate('user_id', 'title firstName lastName author', 'users');
@@ -14,6 +15,7 @@ module.exports = (app, memcache) => {
       match = match.toJSON(); // convert to POJO
       match.author = match.user_id.author; // map author
       delete match.user_id; // remove populated user object
+      memcache.set(cacheKey, match);
       res.send(match);
     } catch (e) {
       next(e);
