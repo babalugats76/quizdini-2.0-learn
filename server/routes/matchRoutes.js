379f98cc3f4fc4696c4c04a6472473a7299aea9d
James Colestock <james@colestock.com>
2020-01-29 14:20:31 -0700

    Commented routes

--- server/routes/matchRoutes.js
+++ server/routes/matchRoutes.js
@@ -5,9 +5,13 @@ module.exports = (app, memcache) => {
   app.get('/api/match/:id', async (req, res, next) => {
     try {
       // throw new Error('Testing match error...');
+
+      // Lookup game in cache; if found, return...
       const cacheKey = `match-${req.params.id}`;
       const cached = await memcache.get(cacheKey);
       if (cached) return res.send(cached);
+      
+      // Otherwise, fetch data from database
       let match = await Match.findOne({
         matchId: req.params.id
       }).populate('user_id', 'title firstName lastName author', 'users');
@@ -15,7 +19,10 @@ module.exports = (app, memcache) => {
       match = match.toJSON(); // convert to POJO
       match.author = match.user_id.author; // map author
       delete match.user_id; // remove populated user object
+      
+      // Write game data to cache
       memcache.set(cacheKey, match);
+      
       res.send(match);
     } catch (e) {
       next(e);
